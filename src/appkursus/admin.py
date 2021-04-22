8c7cb338c1b3d7706cadb57037175ceb2353db4f
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2021-04-22 11:22:40 +0800

    update

--- src/appkursus/admin.py
+++ src/appkursus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Course, Attandance, UnitGroup, Sokongan, UserProfile
+
+
+admin.site.register(Course)
+admin.site.register(Attandance)
+admin.site.register(UnitGroup)
+admin.site.register(Sokongan)
+admin.site.register(UserProfile)
 
 # Register your models here.
--- src/appkursus/models.py
+++ src/appkursus/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=200, null=True, blank=True)
+    course_startdate = models.CharField(max_length=12, null=True, blank=True)
+    course_endate = models.CharField(max_length=12, null=True, blank=True)
+    course_place = models.CharField(max_length=12, null=True, blank=True)
+    course_year = models.CharField(max_length=8, null=True, blank=True)
+    course_days = models.CharField(max_length=20, null=True, blank=True)
+    course_by = models.CharField(max_length=200, null=True, blank=True)
+
+class Attandance(models.Model):
+    dayone = models.CharField(max_length=5, null=True, blank=True)
+    daytwo = models.CharField(max_length=5, null=True, blank=True)
+    daythree = models.CharField(max_length=5, null=True, blank=True) 
+    dayfour = models.CharField(max_length=5, null=True, blank=True)
+    dayfive = models.CharField(max_length=5, null=True, blank=True)
+    daysix = models.CharField(max_length=5, null=True, blank=True)
+    dayseven = models.CharField(max_length=5, null=True, blank=True)
+    dayeight = models.CharField(max_length=5, null=True, blank=True)
+    daynine = models.CharField(max_length=5, null=True, blank=True)
+    dayten = models.CharField(max_length=5, null=True, blank=True)
+    cid = models.ForeignKey(Course, on_delete=models.CASCADE)
+    uid = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class UnitGroup(models.Model):
+    unitname = models.CharField(max_length=50, null=True, blank=True)
+
+class Sokongan(models.Model):
+    sokonganname = models.CharField(max_length=50, null=True, blank=True)
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nobadan = models.CharField(max_length=50, null=True, blank=True)
+    gambar = models.FileField(null=True, blank=True);
+    nokp = models.CharField(max_length=50, null=True, blank=True)
+    balaioperasi = models.CharField(max_length=50, null=True, blank=True) 
+    jawatan = models.CharField(max_length=50, null=True, blank=True)
+    gredjawatan = models.CharField(max_length=50, null=True, blank=True)
+    unitsokongan  = models.ForeignKey(Sokongan, on_delete=models.CASCADE)
+    groupunit = models.ForeignKey(UnitGroup, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.first_name
+
+
+
+    
+
+
 
 # Create your models here.
